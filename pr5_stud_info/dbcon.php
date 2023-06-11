edf11ce640c636a88ff65d60a9db0065fef76bbd
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-06-11 22:30:18 +0530

    Delete dbcon.php

--- pr5_stud_info/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Database Connection<br>";
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$conn = mysqli_connect($hostname,$username,$password);
-echo "Connection Successful";
-?>
